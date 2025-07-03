2b3db5d02a10a2be0091a6bbcef0333d2cff18b5
iclasTH <iclasinprogramacao@gmail.com>
2025-07-03 12:06:32 -0300

    Adicionando as implementações ao sistema

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,29 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando para...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação de...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,73 @@
 ﻿using DesafioPOO.Models;
 
+bool menu = true;
+Console.WriteLine("Qual Smartphone você deseja acessar?\n Nokia ou Iphone");
+string escolha = Console.ReadLine();
+escolha = escolha.ToUpper();
+if (escolha == "NOKIA")
+{
+    while (menu)
+    {
+        Console.Clear();
+        Console.WriteLine("Bem vindo ao seu Nokia!");
+        Smartphone nokia = new Nokia("40028922", "Nokia 3200", "123456789012345", 64);
+        Console.WriteLine("O que deseja fazer?\n 1 - Ligar\n 2 - Receber Ligação\n 3 - Instalar Aplicativo\n 4 - Sair");
+        string opcao = Console.ReadLine();
+        switch (opcao)
+        {
+            case "1":
+                nokia.Ligar();
+                break;
+            case "2":
+                nokia.ReceberLigacao();
+                break;
+            case "3":
+                nokia.InstalarAplicativo("Nokia");
+                break;
+            case "4":
+                menu = false;
+                Console.WriteLine("Saindo do Nokia...");
+                break;
+            default:
+                Console.WriteLine("Opção inválida, tente novamente.");
+                break;
+        }
+    }
+
+} else if (escolha == "IPHONE")
+{
+    while (menu)
+    {
+        Console.Clear();
+        Console.WriteLine("Bem vindo ao seu Iphone!");
+        Smartphone iphone = new Iphone("40028922", "Iphone 13", "123456789012345", 128);
+        Console.WriteLine("O que deseja fazer?\n 1 - Ligar\n 2 - Receber Ligação\n 3 - Instalar Aplicativo\n 4 - Sair");
+        string opcao = Console.ReadLine();
+        switch (opcao)
+        {
+            case "1":
+                iphone.Ligar();
+                break;
+            case "2":
+                iphone.ReceberLigacao();
+                break;
+            case "3":
+                iphone.InstalarAplicativo("Iphone");
+                break;
+            case "4":
+                menu = false;
+                Console.WriteLine("Saindo do Iphone...");
+                break;
+            default:
+                Console.WriteLine("Opção inválida, tente novamente.");
+                break;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Opção inválida, tente novamente.");
+}
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
